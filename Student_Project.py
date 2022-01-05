2aa55bbd4f35bbf696ac5a63e568c46c1dbc807d
Agochar123 <94464716+Agochar123@users.noreply.github.com>
2022-01-05 22:07:05 +0530

    Update Student_Project.py

--- Student_Project.py
+++ Student_Project.py
@@ -1,1 +1,8 @@
-#Write the code over here
+import matplotlib.pyplot as plt
+classes=[1,2,3,4,5,6,7,8,9,10]
+no_of_students=[60,65,60,50,55,40,30,35,40,40]
+plt.xlabel("GRADES")
+plt.ylabel("NUMBER OF STUDENTS ENROLLED")
+plt.title("STUDENTS ENROLLED IN DIFFERENT CLASSES")
+plt.bar(classes,no_of_students,color="maroon")
+plt.show()
